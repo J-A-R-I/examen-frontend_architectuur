1e3561b94f7d830361062ba3ace6c10dc01bed96
Vandeput Jari <vandeputja@gmail.com>
2025-12-01 12:01:49 +0100

    opdracht3

--- oefening3/src/js/main.js
+++ oefening3/src/js/main.js
@@ -5,36 +5,33 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 //eigen js
-const logs = []
-const total = []
 
 const naam_inp = document.getElementById("team_name")
 const value = document.getElementById("team_value")
 
-
-function logList(naam) {
-
-    logs.push(naam_inp.value,  value.value)
+function logList() {
+    const logs = []
+    logs.push(naam_inp.value, value.value)
     console.log(logs)
     const log_list = document.querySelector("#log_list");
-    log_list.innerHTML += `<li class="list-group-item">naam: ${naam_inp.value} <br> waarde: ${value.value} </li>`
+    log_list.innerHTML += `<li class="list-group-item">naam: <strong>${naam_inp.value}</strong> <br> waarde: <strong>${value.value}</strong> </li>`
+
+    totalList(naam_inp.value)
 }
-
-function totalList(){
-
-
-    logs.filter((log) => naam)
-
-
-    total.push()
-    console.log(total)
-    const totals_list = document.querySelector("#totals_list")
-    totals_list.innerHTML += `<li class="list-group-item">${}</li>`
+function totalList(team){
+    const groep = []
+    groep.push(team)
+    const total = document.querySelector("#totals_list")
+    total.innerHTML += `<li class="list-group-item">het aantal van de groep met de naam <strong>${naam_inp.value}</strong> is <strong>${value.value}</strong></li>`
 }
 
 
 
 document.getElementById("btn_send").addEventListener("click", () =>{
-    logList()
-    totalList()
+
+    if(naam_inp.value.trim() === "" || value.value.trim() === ""){
+        alert("vul de vakjes juist in")
+    }else{
+        logList()
+    }
 })
